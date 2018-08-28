05d8583c0bf760c271e5340eb56dfb4e99a36b1e
yigitfindikli <yigitfindikli@gmail.com>
2018-08-28 11:12:25 +0300

    version update
    change primeng version (6.1.2)

--- src/utils/lib-versions.js
+++ src/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.angularVersion = '^6.0.3';
-exports.primengVersion = '^6.1.0';
+exports.primengVersion = '^6.1.2';
 exports.primeiconsVersion = '^1.0.0-beta.10';
 //# sourceMappingURL=lib-versions.js.map
